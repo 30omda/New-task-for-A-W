29a256373e625b92b8d1e446eaff87ab3f27f707
Mohamed ShamsEldeen <74777795+30omda@users.noreply.github.com>
2025-10-29 02:10:50 +0300

    first commit

---
+++ app/sign-in/page.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import LoginForm from '@/components/LoginForm/LoginForm'
+
+
+
+const SignIn = () => {
+    return (
+        <main>
+            <div>SignIn</div>
+            <LoginForm />
+        </main>
+    )
+}
+
+export default SignIn
